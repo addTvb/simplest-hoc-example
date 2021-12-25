2f33b9ac0d4adc079a5b700379fc626bb60a0d42
addTvb <arslan.web.dev@gmail.com>
2021-12-25 15:37:26 +0500

    🐞fix: added emodjies to useEffect dep. variable

--- src/Welcome/WelcomeHOC.js
+++ src/Welcome/WelcomeHOC.js
@@ -3,12 +3,12 @@ import { Welcome } from './Welcome';
 
 export const WelcomeHOC = () => {
 	const emodjies = ['😀', '😁', '😊', '🙂', '😎', '🤩', '🤑', '😇', '🤓', '🤠'];
-	const [emodji, setEmodji] = useState('_');
+	const [emodji, setEmodji] = useState(' ');
 
 	useEffect(() => {
 		const randomIndex = Math.floor(Math.random() * 10);
 		setEmodji(emodjies[randomIndex]);
-	}, []);
+	}, [emodjies]);
 
 	return <Welcome emodji={emodji} />;
 };
